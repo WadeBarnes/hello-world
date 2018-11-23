cd8861e0a88763ddc62b7d787885d732027f241b
Wade Barnes <wade.barnes@shaw.ca>
2018-11-23 07:23:56 -0800

    Integrate working Jenkins tool configuration.
    - Remove unnecessary files.

--- .jenkins/openshift/config.groovy
+++ .jenkins/openshift/config.groovy
@@ -1,9 +1,8 @@
-
 app {
-    name = 'jenkins-cvarjao'
-    namespaces {
+    name = "${opt.'name'?:'jenkins'}"
+    namespaces { //can't call environments :(
         'build'{
-            namespace = 'devex-bcgov-dap-tools'
+            namespace = "${opt.'namespace'?:'devex-bcgov-dap-tools'}"
             disposable = true
         }
         'dev' {
@@ -19,7 +18,7 @@ app {
     git {
         workDir = ['git', 'rev-parse', '--show-toplevel'].execute().text.trim()
         uri = ['git', 'config', '--get', 'remote.origin.url'].execute().text.trim()
-        ref = ['bash','-c', 'git config branch.`git name-rev --name-only HEAD`.merge'].execute().text.trim()
+        ref = "refs/pull/${opt.'pr'}/head"
         commit = ['git', 'rev-parse', 'HEAD'].execute().text.trim()
     }
 
@@ -28,21 +27,26 @@ app {
             name = "build"
             id = "pr-${opt.'pr'}"
         }
-        version = "${app.build.env.name}-v${opt.'pr'}"
-        name = "${opt.'build-name'?:app.name}"
         suffix = "-build-${opt.'pr'}"
         id = "${app.name}${app.build.suffix}"
+        version = "${app.build.env.name}-v${opt.'pr'}"
+        name = "${opt.'build-name'?:app.name}"
+
         namespace = app.namespaces.'build'.namespace
         timeoutInSeconds = 60*20 // 20 minutes
         templates = [
-            [
-                'file':'https://raw.githubusercontent.com/cvarjao-o/openshift-templates/f1d3e44018618ef7c9b9b52dd81d83d7030115db/jenkins/jenkins.bc.yaml',
-                'params':[
-                    'NAME': "${app.build.name}",
-                    'SUFFIX': "${app.build.suffix}",
-                    'VERSION': app.build.version
+                [
+                    'file':'jenkins/openshift/jenkins.bc.json',
+                    'params':[
+                        'NAME': "${app.build.name}",
+                        'SUFFIX': "${app.build.suffix}",
+                        'VERSION': app.build.version,
+                        'SOURCE_REPOSITORY_URL': "${app.git.uri}",
+                        'SOURCE_REPOSITORY_REF': "${app.git.ref}",
+                        'SOURCE_IMAGE_STREAM_NAMESPACE': "bcgov",
+                        'SOURCE_IMAGE_STREAM_TAG': "jenkins-basic:v2-latest"
+                    ]
                 ]
-            ]
         ]
     }
 
@@ -51,36 +55,74 @@ app {
             name = vars.deployment.env.name // env-name
             id = vars.deployment.env.id
         }
-        id = "${app.name}" // app (unique name across all deployments int he namespace)
-        version = "v1" //app-version  and tag
-        name = "${app.name}" //app-name   (same name accross all deployments)
+        suffix = "${vars.deployment.suffix}" // app (unique name across all deployments int he namespace)
+        version = "${vars.deployment.version}" //app-version  and tag
+        name = "${vars.deployment.name}"
+        id = "${app.deployment.name}${app.deployment.suffix}" // app (unique name across all deployments int he namespace)
 
         namespace = "${vars.deployment.namespace}"
         timeoutInSeconds = 60*20 // 20 minutes
+        host = "${app.deployment.id}-${app.deployment.namespace}.pathfinder.gov.bc.ca"
+
         templates = [
-                [
-                    'file':'https://raw.githubusercontent.com/cvarjao-o/openshift-templates/f1d3e44018618ef7c9b9b52dd81d83d7030115db/jenkins/jenkins.dc.yaml',
-                    'params':[
-                        'NAME':app.deployment.name,
-                        'BC_NAME':app.build.name,
-                        'NAME_SUFFIX':'cvarjao',
-                        'VERSION': app.deployment.version,
-                        'ROUTE_HOST': 'jenkinns-hello-cvarjao.pathfinder.gov.bc.ca'
-                    ]
+            [
+                'file':'jenkins/openshift/jenkins.dc.json',
+                'params':[
+                    'NAME': "${app.deployment.name}",
+                    'BC_NAME': "${app.build.name}",
+                    'SUFFIX': "${app.deployment.suffix}",
+                    'VERSION': app.deployment.version,
+                    'ROUTE_HOST': app.deployment.host,
+                    'ENV_NAME':app.deployment.env.name,
+                    'ENV_ID':app.deployment.env.id
                 ]
+            ]
         ]
     }
 }
 
-vars {
-    deployment {
-        env {
-            name ="prod"
-            id = "pr-${opt.'pr'}"
+environments {
+    'dev' {
+        vars {
+            deployment {
+                env {
+                    name ="dev"
+                    id = "pr-${opt.'pr'}"
+                }
+                suffix = "-dev-${opt.'pr'}"
+                name = "${opt.'deployment-name'?:app.name}"
+                namespace = app.namespaces[env.name].namespace
+                version = "${vars.deployment.name}-${vars.deployment.env.name}-v${opt.'pr'}" //app-version  and tag
+            }
         }
-        suffix = ''
-        name = "${opt.'deployment-name'?:app.name}"
-        namespace = app.namespaces[env.name].namespace
-        version = "${vars.deployment.name}-${vars.deployment.env.name}-${opt.'pr'}" //app-version  and tag
+    }
+    'test' {
+        vars {
+            deployment {
+                env {
+                    name ="test"
+                    id = "pr-${opt.'pr'}"
+                }
+                suffix = '-test'
+                name = "${opt.'deployment-name'?:app.name}"
+                namespace = app.namespaces[env.name].namespace
+                version = "${vars.deployment.name}-${vars.deployment.env.name}" //app-version  and tag
+            }
+        }
+    }
+    'prod' {
+        vars {
+            deployment {
+                env {
+                    name ="prod"
+                    id = "pr-${opt.'pr'}"
+                }
+                suffix = ''
+                id = "${app.name}${vars.deployment.suffix}"
+                name = "${opt.'deployment-name'?:app.name}"
+                namespace = app.namespaces[env.name].namespace
+                version = "${vars.deployment.name}-${vars.deployment.env.name}" //app-version  and tag
+            }
+        }
     }
 }
